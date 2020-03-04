4dc60a50114e30440c32a025208c242f2d234ee9
ald2424 <abigaild2424@gmail.com>
2020-03-04 18:20:39 -0500

    added radio buttons. functionality isn't complete

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -8,27 +8,51 @@ class App extends Component {
 
   state = {
     employees,
-    search: ""
+    search: "",
+    selectedOption: 'all'
   };
 
   updateSearch = e => {
     this.setState({
-      search: e.target.value
+      search: e.target.value,
+      selectedOption: e.target.value
     });
   };
 
+  
 
   render() {
     let filteredEmps = this.state.employees.filter(
       (employee) => {
-        return employee.name.toLowerCase().indexOf(this.state.search.toLocaleLowerCase()) != -1;
-      }
+        return employee.name.toLowerCase().indexOf(this.state.search.toLowerCase()) != -1;      
+    }
     );
+    
     return (
+      // search bar
       <div>
-    {/* <SearchBar search={this.state.search}
-          handleInputChange={this.handleInputChange}/> */}
     <input type='text' onChange={this.updateSearch}/>
+    {/* *********************************************/}
+
+    {/* Radio Buttons */}
+    <form>      
+        <label>
+          <input type = "radio" value="all" checked = {this.state.selectedOption === 'all'} onClick={this.updateSearch}/>
+            View All Employees
+        </label>
+        <label>
+          <input type = "radio" value="full" checked = {this.state.selectedOption === 'full'}onClick={this.updateSearch}/>
+            View Full Time Employees
+        </label>
+        <label>
+          <input type = "radio" value="part" checked = {this.state.selectedOption === 'part'}onClick={this.updateSearch}/>
+            View Part Time Employees
+        </label>
+    </form>
+    
+    {/* ****************************************************** */}
+
+    {/* List of Employees */}
     <Wrapper>
         {filteredEmps.map(employee => (
          <EmpInfo
@@ -40,6 +64,7 @@ class App extends Component {
           />
         ))}
         </Wrapper>
+      {/* *********************************************************** */}
       </div>
     );
   }
--- employee-directory/src/components/SearchBar/index.js
+++ employee-directory/src/components/SearchBar/index.js
@@ -5,15 +5,7 @@ function searchBar(props){
     return(
         
         <div>
-        <label htmlFor="search">Search:</label>
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          placeholder="Search for an Employee"
-          id="search"
-        />
+        
       </div>
         
     )
