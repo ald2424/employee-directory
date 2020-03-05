48e1ff12bd5061b048c0e73433b5e9077bacc30b
ald2424 <abigaild2424@gmail.com>
2020-03-04 20:49:09 -0500

    Radio buttons are functioning.  Added a sort button

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import EmpInfo from "./components/EmployeeInfo";
 import Wrapper from "./components/Wrapper";
 import SearchBar from "./components/SearchBar";
+import RadioOptions from "./components/RadioOptions";
 import employees from "./employees.json";
 
 class App extends Component {
@@ -9,46 +10,59 @@ class App extends Component {
   state = {
     employees,
     search: "",
-    selectedOption: 'all'
+    selectedOption: 'all',
   };
 
   updateSearch = e => {
+    let name = e.target.name;
     this.setState({
-      search: e.target.value,
-      selectedOption: e.target.value
+      [name]: e.target.value
     });
   };
 
-  
+  sort = e => {
+    let copy = [...this.state.employees];
+    copy.sort((a,b)=>{
+      var nameA = a.name.toUpperCase(); 
+      var nameB = b.name.toUpperCase(); 
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      return 0;
+    })
+    this.setState({employees: copy})
+  }
+
 
   render() {
     let filteredEmps = this.state.employees.filter(
       (employee) => {
-        return employee.name.toLowerCase().indexOf(this.state.search.toLowerCase()) != -1;      
+        let checked;
+        if(this.state.selectedOption === "all" || this.state.selectedOption === employee.empType) checked = true;
+        else checked = false;
+
+        //let checked = this.state.selectedOption === "all" || this.state.selectedOption === employee.empType
+
+        return employee.name.toLowerCase().indexOf(this.state.search.toLowerCase()) != -1 && checked;      
     }
     );
     
     return (
       // search bar
       <div>
-    <input type='text' onChange={this.updateSearch}/>
+        <button onClick={this.sort}>Sort</button>
+    <input type='text' name="search" onChange={this.updateSearch}/>
     {/* *********************************************/}
 
     {/* Radio Buttons */}
-    <form>      
-        <label>
-          <input type = "radio" value="all" checked = {this.state.selectedOption === 'all'} onClick={this.updateSearch}/>
-            View All Employees
-        </label>
-        <label>
-          <input type = "radio" value="full" checked = {this.state.selectedOption === 'full'}onClick={this.updateSearch}/>
-            View Full Time Employees
-        </label>
-        <label>
-          <input type = "radio" value="part" checked = {this.state.selectedOption === 'part'}onClick={this.updateSearch}/>
-            View Part Time Employees
-        </label>
-    </form>
+    <RadioOptions
+      selectedOption={this.state.selectedOption}
+      updateSearch = {this.updateSearch}
+    />
     
     {/* ****************************************************** */}
 
